c102e8aa19aebe740cac15b20dcd94e79fafbc3a
mohnalkhateeb <mhmmd.alkateeb@gmail.com>
2021-07-11 19:12:35 +0300

    lab11-env

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-// TODO: wrap everything in Auth0
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain= 'dev-qjc40gb9.us.auth0.com' //{process.env.DOMAIN}
+    clientId= 'rxIXUFO6Ba0vVsgfEJyvS7KiwQniwjug' //{process.env.CLIENT_ID}
+    redirectUri={window.location.origin}
+    
+  >
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Auth0Provider>,
+  document.getElementById("root")
 );
